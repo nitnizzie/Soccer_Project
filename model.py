e2ed2c25bf058c428c298cfbf2681c8e6adaac39
vonnish <shinhyuk1001@kaist.ac.kr>
2020-11-24 15:54:05 +0900

    model.py updated

--- model.py
+++ model.py
@@ -17,15 +17,26 @@ import torch.nn.functional as F
 #Network Architectures
 class DQN(nn.Module):
 
-    def __init__(self, ,state_dim, action_dim):
+    def __init__(self, ,input_shape, action_dim):
+        '''
+        input shape: (C, H, W) = (3, 14, 8)
+        action_dim: 3
+        '''
         super(DQN, self).__init__()
-        self.conv1 = nn.Conv2d(state_dim, 16, kernel_size=5, stride=2)
+        self.conv1 = nn.Conv2d(input_shape[0], 16, kernel_size=5, stride=2)
         self.bn1 = nn.BatchNorm2d(16)
         self.conv2 = nn.Conv2d(16, 32, kernel_size=5, stride=2)
         self.bn2 = nn.BatchNorm2d(32)
         self.conv3 = nn.Conv2d(32, 32, kernel_size=5, stride=2)
         self.bn3 = nn.BatchNorm2d(32)
-        self.fc4 = nn.Linear( * n * 32, 256)
+
+        def conv2d_size_out(size, kernel_size = 2, stride = 1):
+            return (size - (kernel_size - 1) - 1) // stride  + 1
+
+        self.convw = conv2d_size_out(conv2d_size_out(conv2d_size_out(input_shape[1])))
+        self.convh = conv2d_size_out(conv2d_size_out(conv2d_size_out(input_shape[2])))
+
+        self.fc4 = nn.Linear(convw * convh * 32, 256)
         self.fc5 = nn.Linear(256, action_dim)
 
     def forward(self, x):
