b0d14326f2eddf557fbd40fba84cb3b12e674e7f
vonnish <shinhyuk1001@kaist.ac.kr>
2020-11-24 15:46:24 +0900

    utils.py updated

--- train.py
+++ train.py
@@ -40,16 +40,11 @@ behavior_name_2 = list(env.behavior_specs)[1]
 decision_steps_p, terminal_steps_p = env.get_steps(behavior_name_1)
 decision_steps_b, terminal_steps_b = env.get_steps(behavior_name_2)
 
-# front_b1, back_b1, front_b2, back_b2, reward_b = step(decision_steps_b)
-# front_p1, back_p1, front_p2, back_p2, reward_p = step(decision_steps_p)
-state_b, reward_b = step(decision_steps_b)
-state_p, reward_p = step(decision_steps_p)
+state_b1, state_b2, reward_b = step(decision_steps_b)
+state_p1, state_p2, reward_p = step(decision_steps_p)
 
-
-
-
-agent_b = Agent(state_dim=5, action_dim=1, device=device)
-agent_p = Agent(state_dim=5, action_dim=1, device=device)
+agent_b = Agent(state_dim=3, action_dim=3, device=device)
+agent_p = Agent(state_dim=3, action_dim=3, device=device)
 
 best_reward = -np.inf
 saved_reward = -np.inf
@@ -68,37 +63,33 @@ for episode in range(num_episodes):
     decision_steps_p, terminal_steps_p = env.get_steps(behavior_name_1)
     decision_steps_b, terminal_steps_b = env.get_steps(behavior_name_2)
     
-    # front_b1, back_b1, front_b2, back_b2, reward_b = step(decision_steps_b)
-    # front_p1, back_p1, front_p2, back_p2, reward_p = step(decision_steps_p)
-    state_b, reward_b = step(decision_steps_b)
-    state_p, reward_p = step(decision_steps_p)
+    state_b1, state_b2, reward_b = step(decision_steps_b)
+    state_p1, state_p2, reward_p = step(decision_steps_p)
 
     for step in range(num_steps):
         global_step += 1
 
         #select action
-        action_b = agent_b.select_action(state, step)
-        action_p = agent_b.select_action(state, step)
+        action_b = agent_b.select_action(state, step)   #(2,3): [(a1, a2, a3), (b1, b2, b3)]
+        action_p = agent_p.select_action(state, step)   #(2,3)
 
         #Execute action a_t
         env.set_actions(behavior_name_1, action_p) #p
         env.set_actions(behavior_name_2, action_b) #b
-        # env.set_actions(behavior_name_1, np.array([(1,0,0),(2,0,0)])) #p
-        # env.set_actions(behavior_name_2, np.array([(0,0,1),(0,1,0)])) #b
         env.step()
 
         #Observe reward r_t and next state s_(t+1)
         decision_steps_p, terminal_steps_p = env.get_steps(behavior_name_1)
         decision_steps_b, terminal_steps_b = env.get_steps(behavior_name_2)
         
-        # front_b1, back_b1, front_b2, back_b2, reward_b = step(decision_steps_b)
-        # front_p1, back_p1, front_p2, back_p2, reward_p = step(decision_steps_p)
-        next_state_b, reward_b = step(decision_steps_b)
-        next_state_p, reward_p = step(decision_steps_p)
+        next_state_b1, next_state_b2, reward_b = step(decision_steps_b)
+        next_state_p1, next_state_p2, reward_p = step(decision_steps_p)
 
         #Store Transition to Memory
-        agent_b.store_transtion(state_b, action, reward_b, next_state_b)
-        agent_p.store_transtion(state_p, action, reward_p, next_state_p)
+        agent_b.store_transtion(state_b1, action_b1, reward_b, next_state_b)
+        agent_b.store_transtion(state_b2, action_b2, reward_b, next_state_b)
+        agent_p.store_transtion(state_p1, action_p1, reward_p, next_state_p1)
+        agent_p.store_transtion(state_p2, action_p2, reward_p, next_state_p2)
 
         #train agent
         agent_b.train()
