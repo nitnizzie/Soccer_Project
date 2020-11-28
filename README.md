d0f38b12fca46987076a8e1def310b176fcbaf01
vonnish <shinhyuk1001@kaist.ac.kr>
2020-11-28 21:33:46 +0900

    ppo

---
+++ README.md
@@ -0,0 +1,9 @@
+# Soccer_Project
+CoE202 Soccer Project with unity ml-agents
+
+- Contributors
+1. 20170742 황신혁
+2. 20180645 조유진
+3. 20200493 이재현
+4. 20200497 이정훈
+5. 20200533 이훈민
---
+++ utils.py
@@ -0,0 +1,43 @@
+import numpy as np
+from mlagents_envs.base_env import DecisionSteps
+
+#preprocess sensor data
+def sensor_front_sig(data):
+    player=[]
+    sensor_data=[]
+    for sensor in range(33):
+        player.append(data[8*sensor:(8*sensor)+8])
+    
+    for stack in range(3):
+        sensor_data.append(player[11*stack:(11*stack)+11])
+
+    return sensor_data
+
+def sensor_back_sig(data):
+    player=[]
+    sensor_data=[]
+    for sensor in range(9):
+        player.append(data[8*sensor:(8*sensor)+8])
+    
+    for stack in range(3):
+        sensor_data.append(player[3*stack:(3*stack)+3])
+
+    return sensor_data
+
+def step(decision_steps):
+
+    #Get Signal From Agent 1
+    signal_front_1 = np.array(sensor_front_sig(decision_steps.obs[0][0,:]))   #(3, 11, 8)
+    signal_back_1 = np.array(sensor_back_sig(decision_steps.obs[1][0,:]))     #(3, 3, 8)
+
+    #Get Signal From Agent 2
+    signal_front_2 = np.array(sensor_front_sig(decision_steps.obs[0][1,:]))    #(3, 11, 8)
+    signal_back_2 = np.array(sensor_back_sig(decision_steps.obs[1][1,:]))      #(3, 3, 8)
+
+    #preprocess state
+    state_1 = np.concatenate((signal_front_1, signal_back_1), axis=1)         #(3, 14, 8)
+    state_2 = np.concatenate((signal_front_2, signal_back_2), axis=1)         #(3, 14, 8)
+
+    reward = np.array(decision_steps.reward)
+
+    return state_1, state_2, reward
